ff776898ab9b8e31e99fb2237371f17f40f5ef8a
786384149@qq.com <786384149@qq.com>
2018-01-03 12:24:31 +0800

    wang

--- src/com/pinker/servlet/CollectionBlogServlet.java
+++ src/com/pinker/servlet/CollectionBlogServlet.java
@@ -2,7 +2,7 @@ package com.pinker.servlet;
 
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name = "CollectionBlogServlet" ,urlPatterns = {"CollectionBlogServlet"})
+@WebServlet(name = "CollectionBlogServlet" ,urlPatterns = {"/CollectionBlogServlet"})
 public class CollectionBlogServlet extends BaseServlet {
 
 }
